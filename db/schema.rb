7b95fae83e79346940c1e10d071daefa4c31dfd1
yuuffff1212 <75609235+yuuffff1212@users.noreply.github.com>
2020-12-25 19:46:52 +0900

    ユーザー認証機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_12_23_133702) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "users", null: false
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
